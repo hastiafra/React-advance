2192c881e91a81b92b9cc20e2cbb6069c81add8a
hastiafra <77177165+hastiafra@users.noreply.github.com>
2022-06-21 15:18:30 -0400

    passed all the witten test

--- redux/src/counter/Counter.jsx
+++ redux/src/counter/Counter.jsx
@@ -1,9 +1,17 @@
-function Counter() {
-    return <div>
-        <h2 data-testid ="header">
-            My Counter
-        </h2>
-    </div>;
-  }
-  
-  export default Counter;
+import { useState } from "react";
+
+const Counter = () => {
+  const [counterValue, setCounterValue] = useState(0);
+  const [inputValue, setInputValue] = useState(1)
+  return (
+    <div>
+      <h2 data-testid="header">My Counter</h2>
+      <h3 data-testid="counter">{counterValue}</h3>
+      <button data-testid="subtract-btn">-</button>
+      <input type="number" data-testid="input" value={inputValue} />
+      <button data-testid="add-btn">+</button>
+    </div>
+  );
+};
+
+export default Counter;
--- redux/src/counter/__test__/counter.test.js
+++ redux/src/counter/__test__/counter.test.js
@@ -9,34 +9,31 @@ test("header renders with correct text", () => {
   expect(headerEl.textContent).toBe("My Counter");
 });
 
-
 //
 test("counter initialy starts with text of 0", () => {
   const { getByTestId } = render(<Counter />);
-  const counterEl = component.getByTestId("counter");
+  const counterEl = getByTestId("counter");
   expect(counterEl.textContent).toBe("0");
 });
 
 test("input initial value starts at 1", () => {
   const { getByTestId } = render(<Counter />);
-  const inputEl = component.getByTestId("input");
+  const inputEl = getByTestId("input");
 
   //because it it is input we right value instead of text
   expect(inputEl.value).toBe("1");
 });
 
 test("add button renders with +", () => {
-    const { getByTestId } = render(<Counter />);
-    const addBtn = component.getByTestId("add-subtract");
-  
-    expect(addBtn.textContent).toBe("+");
-  });
+  const { getByTestId } = render(<Counter />);
+  const addBtn = getByTestId("add-btn");
 
-  
+  expect(addBtn.textContent).toBe("+");
+});
+
 test("subtract button renders -", () => {
-    const { getByTestId } = render(<Counter />);
-    const subtractBtn = component.getByTestId("add-subtract");
-  
-    expect(subtractBtn.textContent).toBe("+");
-  });
-  
+  const { getByTestId } = render(<Counter />);
+  const subtractBtn = getByTestId("subtract-btn");
+
+  expect(subtractBtn.textContent).toBe("-");
+});
